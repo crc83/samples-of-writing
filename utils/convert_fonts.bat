7df7f99b208582c91d544efc2d8746c2eee5e100
Sergiy Beley <Sergiy.Beley@gmail.com>
2017-05-03 01:09:48 +0300

    moved to github.io (last changes here

--- utils/convert_fonts.bat
+++ utils/convert_fonts.bat
@@ -1,5 +1,7 @@
 setlocal
 set FONTFORGE_PATH=c:\Program Files (x86)\FontForgeBuilds
 call "%FONTFORGE_PATH%\fontforge.bat" -lang=py -script fontconvert.py ..\fonts-sfd\SchoolLight_UAwords-Regular.sfd -w
-copy ..\fonts-sfd\SchoolLight_UAwords-Regular.woff  ..\css\fonts\school.woff /Y
+move /Y ..\fonts-sfd\SchoolLight_UAwords-Regular.woff  ..\css\fonts\school.woff
+call "%FONTFORGE_PATH%\fontforge.bat" -lang=py -script fontconvert.py ..\fonts-sfd\SchoolLight_punktir.sfd -w
+move /Y ..\fonts-sfd\SchoolLight_punktir.woff  ..\css\fonts\school-dash.woff
 endlocal
